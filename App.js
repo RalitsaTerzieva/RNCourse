9e48cadc7873d16e6b7b2046794ec4fc5495abd6
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-15 14:51:32 +0500

    Add first Button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My First React Native!</Text>
+      <Button title='Click me!'  />
     </View>
   );
 }
