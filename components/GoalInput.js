c62d9b61cbcffdd535a7cf3b3c544321feee3f47
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-17 17:02:52 +0500

    Add children components

---
+++ components/GoalInput.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, Button } from 'react-native';
+import { useState } from 'react';
+
+function GoalInput(props) {
+
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText)
+    }
+
+    function addGoalHandler() {
+        props.onAddGoal(enteredGoalText)
+    }
+
+    return (
+    <View style={styles.inputContainer} >
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
+        <Button title='Add goal' onPress={props.onAddGoal} />
+    </View>
+    )
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row',
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+});
