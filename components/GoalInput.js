982e24acabbe8c0bcd992c66788e1c3f8d85b117
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-25 16:34:16 +0300

    Change colors

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -26,10 +26,10 @@ function GoalInput(props) {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title='Cancel' onPress={props.onCancel} color='#f31282'/>
+                        <Button title='Cancel' onPress={props.onCancel} color='#b180f0'/>
                     </View>
                     <View style={styles.button}>
-                        <Button title='Add goal' onPress={addGoalHandler} color='#b180f0' />
+                        <Button title='Add goal' onPress={addGoalHandler} color='#f31282' />
                     </View>
                 </View>
             </View>
